8d6ed3aa1077a42c869f2c711b3ac392985c3a21
wssommerhauser <130407147+wssommerhauser@users.noreply.github.com>
2023-09-17 22:45:23 -0500

    Add files via upload

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,13 +18,19 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+    candidateName = input.question("Please input your name: ");
+    console.log("Hello, " + candidateName + "!");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+    candidateAnswer = input.question(question);
+    if (candidateAnswer === correctAnswer){
+        console.log("That is correct!");
+    }
+    else{
+        console.log("That is incorrect.");
+    }
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -46,7 +52,7 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
-
+console.log(runProgram());
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
